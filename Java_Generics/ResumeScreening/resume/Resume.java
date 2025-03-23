0e2eba8f8112a322f7c2680d69c06cb7bae719c1
Rishav1167 <Rishav1167>
2025-03-23 23:34:58 +0530

    [Rishav] Add: Implemented AI-Driven Resume Screening System

--- Java_Generics/ResumeScreening/resume/Resume.java
+++ Java_Generics/ResumeScreening/resume/Resume.java
@@ -1,4 +1,22 @@
 package Java_Generics.ResumeScreening.resume;
 
-public class Resume {
+import Java_Generics.ResumeScreening.job_roles.JobRole;
+
+public class Resume <T extends JobRole>{
+    private T jobRole;
+
+    public Resume(T jobRole) {
+        this.jobRole = jobRole;
+    }
+
+    public T getJobRole() {
+        return jobRole;
+    }
+    public void setJobRole(T jobRole) {
+        this.jobRole = jobRole;
+    }
+
+    public void screen(){
+        System.out.println(jobRole);
+    }
 }
