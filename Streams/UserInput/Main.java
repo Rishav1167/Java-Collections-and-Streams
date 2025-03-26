9c861ba7a950b2622a96c7615dfdfdf4cb6e1fb7
Rishav1167 <Rishav1167>
2025-03-26 10:15:22 +0530

    [Rishav] Add :  Read User Input from Console

--- Streams/UserInput/Main.java
+++ Streams/UserInput/Main.java
@@ -1,4 +1,12 @@
 package Streams.UserInput;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.print("Enter your name: ");
+        String name = FileHandler.readConsole();
+        System.out.print("Enter your age: ");
+        String age = FileHandler.readConsole();
+        String dest="C:\\Users\\risha\\OneDrive\\Desktop\\BridgeLabz_WorkSpace\\Java_Collections_And_Streams\\src\\Streams\\UserInput\\content\\output.text";
+        FileHandler.writeFile(dest, "Name: " + name + "\nAge: " + age);
+    }
 }
