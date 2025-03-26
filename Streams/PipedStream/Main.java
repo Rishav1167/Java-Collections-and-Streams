d8bfb964e1b6b027e6661d270de30d5dd05e7415
Rishav1167 <Rishav1167>
2025-03-26 10:12:40 +0530

    [Rishav] Add : Implemented Inter-Thread Communication

--- Streams/PipedStream/Main.java
+++ Streams/PipedStream/Main.java
@@ -1,4 +1,20 @@
 package Streams.PipedStream;
 
+import Streams.PipedStream.tasks.*;
+
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        PipedOutputStream pos = new PipedOutputStream();
+        PipedInputStream pis = new PipedInputStream(pos);
+
+        // Create and start writer and reader threads
+        Thread writerThread = new Thread(new WriterTask(pos));
+        Thread readerThread = new Thread(new ReaderTask(pis));
+
+        writerThread.start();
+        readerThread.start();
+    }
 }
